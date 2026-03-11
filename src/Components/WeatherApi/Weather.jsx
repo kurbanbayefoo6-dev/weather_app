439311342cd483078eb57f4222002481c6c10621
kurbanbayefoo6-dev <kurbanbayefoo6@icloud.com>
2026-03-11 10:25:53 +0500

    Update Weather.jsx

--- src/Components/WeatherApi/Weather.jsx
+++ src/Components/WeatherApi/Weather.jsx
@@ -7,7 +7,7 @@ export default function Weather() {
 	const [error, setError] = useState(null)
 
 	useEffect(() => {
-		fetch('https://api.weatherapi.com/v1/forecast.json?q=Tashkent&key=e0c4fcc2ca0449578ae35836241312')
+		fetch('/.netlify/functions/weather?city=Tashkent')
 			.then(response => {
 				if (!response.ok) {
 					throw new Error(`Serverdan xato status qaytdi: ${response.status}`)
@@ -36,7 +36,7 @@ export default function Weather() {
 		)
 	}
 
-	// Yuklanish 
+	// Yuklanish
 	if (!weatherData) {
 		return (
 			<div className='page'>
